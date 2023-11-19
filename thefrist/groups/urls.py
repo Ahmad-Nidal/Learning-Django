d7298f6eaec58b50c910c9b9faa2a15c8b46db7a
Ahmad Nidal <ahmadnidal.dr@gmail.com>
2023-11-19 10:25:32 +0300

    Url Grouping

--- thefrist/groups/urls.py
+++ thefrist/groups/urls.py
@@ -1,12 +1,15 @@
-
-from django.urls import path
+from django.urls import path, include
 from . import views
 
-
 urlpatterns = [
-    path('groups/', views.groups, name='groups'),
-    path('groups/detail/<int:id>/', views.detail, name='detail'),
-    path('groups/create/', views.create, name='create'),
-    path('groups/update/<int:id>/', views.update, name='update'),
-    path('groups/delete/<int:id>/', views.delete, name='delete'),
+    path("groups/",
+        include(
+        [
+            path("", views.groups, name="groups"),
+            path("detail/<int:id>/", views.detail, name="detail"),
+            path("create/", views.create, name="create"),
+            path("update/<int:id>/", views.update, name="update"),
+            path("delete/<int:id>/", views.delete, name="delete"),
+        ]
+    ))
 ]
